5e0253ce3206ba10c76bdce2c6382529e60cc343
Sebastian Reuße <seb@wirrsal.net>
2019-09-21 09:27:58 +0200

    Bump pygobject dependency bounds

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     url='https://github.com/eigengrau/flash-screen',
     packages=['flash_screen'],
     package_dir={'': 'src'},
-    install_requires=['pygobject >= 3.18, < 3.29'],
+    install_requires=['pygobject >= 3.18, < 3.33'],
     license="GPL3",
     entry_points={
         'console_scripts': [

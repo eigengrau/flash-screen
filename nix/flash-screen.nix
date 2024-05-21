240ec855a2a5582e1526435b6b51589460fcc8fe
Sebastian Reuße <seb@schattenkopie.de>
2024-05-21 07:46:10 +0200

    build: depend on gobject-introspection

--- nix/flash-screen.nix
+++ nix/flash-screen.nix
@@ -2,7 +2,7 @@
 python3Packages.buildPythonApplication {
   pname = "flash-screen";
   version = "0.2.1.0";
-  nativeBuildInputs = [ wrapGAppsHook ];
+  nativeBuildInputs = [ wrapGAppsHook gobject-introspection ];
   propagatedBuildInputs = [ python3Packages.pygobject3 ]
     ++ [ gtk3 gobject-introspection gsound ];
   src = ../.;
